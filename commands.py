e065bbef02afd54f3c19f4e8e785e682d2d640ac
kisstopherr <128765312+kisstopherr@users.noreply.github.com>
2024-06-05 20:32:09 -0500

    Add files via upload

---
+++ commands.py
@@ -0,0 +1,31 @@
+import os
+
+file_path = "C:/Program Files (x86)/Steam/steamapps/common/Team Fortress 2/tf/console_chatlog.txt"
+
+def loadCommand(functionName):
+    try:
+        globals()[functionName]()
+        #print(f"Command '{functionName}' executed successfully.")
+    except KeyError:
+        print(f"Function '{functionName}' is not defined.")
+    except Exception as e:
+        print(f"An error occurred while executing '{functionName}': {e}")
+
+def clearChatLog():
+    if not os.path.exists(file_path):
+        print(f"The file {file_path} does not exist.")
+        return
+    
+    with open(file_path, 'w', encoding='utf-8') as file:
+        file.write("")
+
+
+#COMMAND FUNCTIONS-----------------
+
+def ping():
+    print("PONG")
+    clearChatLog()
+
+def help():
+    print("GITHUB")
+    clearChatLog()
---
+++ main.py
@@ -0,0 +1,44 @@
+import time
+import json
+import os
+import commands
+
+username = "kisstopherr"
+botPrefix = "/"
+commandChat = f"{username} :  {botPrefix}"
+currentCommand = ""
+
+def readChatLog(file_path):
+    if not os.path.exists(file_path):
+        print(f"The file {file_path} does not exist.")
+        return
+    
+    with open(file_path, 'r', encoding='utf-8') as file:
+        lines = file.readlines()
+        for line in lines:
+            if commandChat in line:
+                commandLine = line.strip()
+                currentCommand = commandLine[len(commandChat):].strip()
+                #print("/" + currentCommand)
+                findCommands(currentCommand)
+
+def findCommands(command_string):
+    with open('commands.json', 'r') as file:
+        commands_data = json.load(file)
+
+    for command_data in commands_data["commands"]:
+        for command_name, command_info in command_data.items():
+            if command_string == command_name:
+                function_name = command_info["function"]
+                commands.loadCommand(function_name)
+                #print("Command ran:", function_name)
+                return True
+    #print("Command not found")
+    return False
+
+if __name__ == "__main__":
+    log_file = "C:/Program Files (x86)/Steam/steamapps/common/Team Fortress 2/tf/console_chatlog.txt"
+    
+    while True:
+        readChatLog(log_file)
+        time.sleep(1)
